79d3c42dd3c01976432ce8b2ef108f74d197906b
berriosa13 <berriosa13@gmail.com>
2021-05-29 09:16:24 -0400

    updated server file

--- server/server.js
+++ server/server.js
@@ -7,6 +7,31 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post('./refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirect: 'localhost:3000',
+        clientId: 'a48fc48de6f042adaae96306d3875be0',
+        clientSecret: '3bc63cea8ec74d11800505ef1a674506',
+        refreshToken
+    })
+    // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
+spotifyApi.refreshAccessToken().then(
+    data => {
+      console.log('The access token has been refreshed!');
+  
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+    }).catch(() => {
+        res.status(400)
+    })
+    function(err) {
+      console.log('Could not refresh access token', err);
+    }
+  );
+  
+})
+
 // hard coding client ID's
 // to test login page before moving to an env file
 app.post('/login',(req, res) => {
